f3b20a483b1e4ff0f7e77d8b69cf97538f70de15
usein <useinakbar01@gmail.com>
2024-02-20 00:43:57 +0700

    change navbar

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
             </Script>
             <div className='flex items-center gap-4'>
                 <h2>{data && data.user.fullname}</h2>
-                {data.user.image && <Image width={200} height={200} src={data.user.image} alt={data.user.fullname} className='rounded-full w-12 h-12' />}
+                {data?.user?.image && <Image width={200} height={200} src={data.user.image} alt={data.user.fullname} className='rounded-full w-12 h-12' />}
                 {data ? 
                 <button onClick={() => signOut()} className='bg-white text-black font-medium rounded-md px-4 py-2'>Sign Out</button> :
                 // tiap button di klik Sign In, maka method signIn() next-auth udh otomatis create callbackUrl-nya + redirect ke custom page loginnya yg udh diset di [...nextauth].ts
